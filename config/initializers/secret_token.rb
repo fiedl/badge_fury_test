a26082e024f48098377a1e6aed75a928703f9bcd
Sebastian Fiedlschuster <sebastian@fiedlschuster.de>
2013-12-28 13:11:48 +0100

    adding rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BadgeFuryTest::Application.config.secret_key_base = '78999f3a80ee22f2128b3c30bde06f82f433faf58f001487e3bcdc2d0a2f5e627a3222bca38baa33d1751687b38cf876d9a4afe90e7fccf1ea3c9e0253728429'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BadgeFuryTest::Application.config.session_store :cookie_store, key: '_badge_fury_test_session'
